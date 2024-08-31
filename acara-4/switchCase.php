fdc5a666f35511129cd6472b3e647d7e72b04fd5
IlmanNafi11 <ilmannafi04@gmail.com>
2024-09-01 05:39:05 +0700

    membuat contoh switch case

---
+++ acara-4/switchCase.php
@@ -0,0 +1,20 @@
+<?php
+$role = "sales";
+switch ($role) {
+    case "admin":
+        echo "Anda login sebagai admin";
+        break;
+    case "teknisi":
+        echo "Anda login sebagai teknisi";
+        break;
+    case "sales":
+        echo "Anda login sebagai sales";
+        break;
+    default:
+        echo "Role tidak dikenali";
+}
+?>Nama		: ILMAN NAFIAN
+NIM		: E41231969
+Golongan	: C
+ACARA 4
+Perulangan pada PHP

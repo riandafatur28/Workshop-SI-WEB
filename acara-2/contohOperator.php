ee4ce405fda4c2db80acc1dbb18af11f25c9235d
IlmanNafi11 <ilmannafi04@gmail.com>
2024-09-01 01:21:30 +0700

    membuat contoh penggunaan operator

---
+++ acara-2/contohOperator.php
@@ -0,0 +1,10 @@
+<?php
+// Contoh penggunaan operator ("/")
+$angka1 = 10;
+$angka2 = 2;
+echo $angka1/$angka2; // hasilnya 10 / 2 = 5
+
+// Contoh penggunaan operator ("%")
+$angka3 = 7;
+$angka4 = 2;
+echo $angka3 % $angka4; // hasilnya 7 % 2 = 1

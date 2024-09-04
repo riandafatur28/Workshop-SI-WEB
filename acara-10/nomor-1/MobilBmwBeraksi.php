d080bb8ac1be0fae724aaa0cf89be6f44a50b2bb
IlmanNafi11 <ilmannafi04@gmail.com>
2024-09-04 13:31:45 +0700

    penerapan inherit pada mobil lengkap

---
+++ acara-10/nomor-1/MobilBmwBeraksi.php
@@ -0,0 +1,18 @@
+<?php
+class MobilBMWberaksi {
+    public function nontonTV(MobilBMW $mobil) {
+        $mobil->nontonTV();
+    }
+
+    public function hidupkanMobil(MobilBMW $mobil) {
+        $mobil->hidupkanMobil();
+    }
+
+    public function matikanMobil(MobilBMW $mobil) {
+        $mobil->matikanMobil();
+    }
+
+    public function ubahGigi(MobilBMW $mobil) {
+        $mobil->ubahGigi();
+    }
+}
